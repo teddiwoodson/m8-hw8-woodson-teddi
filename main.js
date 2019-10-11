553c286bafb3488ebd6d292f52e4863de9170603
Teddi Woodson <teddiwood97@ufl.edu>
2019-10-11 14:44:59 -0400

    Add transitions to dropdown/add dissapear

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 // It is always helpful to use comments in your code!
 var productsTab = document.getElementById('products');
 var productsOptions = document.querySelector('.products');
-
-function toggleMenu() {
+//dropdown
+function dropdown() {
   productsOptions.classList.add('show-menu');
 }
-
-productsTab.addEventListener('mouseover', toggleMenu);
+productsTab.addEventListener('mouseover', dropdown);
+//dissappear
+function dissapear() {
+  productsOptions.classList.remove('show-menu');
+}
+productsTab.addEventListener('mouseleave', dissapear);
