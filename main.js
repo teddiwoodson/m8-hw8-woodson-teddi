c750260ccc2d1b7940bb99a3d45eea1a59112f9c
Teddi Woodson <teddiwood97@ufl.edu>
2019-10-11 14:57:35 -0400

    Add js functionality to company

--- main.js
+++ main.js
@@ -28,3 +28,17 @@ function dissapear2() {
   servicesOptions.classList.remove('show-menu');
 }
 services.addEventListener('mouseleave', dissapear2);
+
+//services
+var companyTab = document.getElementById('company');
+var companyOptions = document.querySelector('.company');
+//dropdown
+function dropdown3() {
+  companyOptions.classList.add('show-menu');
+}
+companyTab.addEventListener('mouseover', dropdown3);
+//dissappear
+function dissapear3() {
+  companyOptions.classList.remove('show-menu');
+}
+company.addEventListener('mouseleave', dissapear3);
