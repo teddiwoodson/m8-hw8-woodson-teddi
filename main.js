6cd363d9bdb16854072e758ffd9111fa706662df
Teddi Woodson <teddiwood97@ufl.edu>
2019-10-11 14:31:07 -0400

    Hide products sub-menu

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 // It is always helpful to use comments in your code!
 var productsTab = document.getElementById('products');
 var productsOptions = document.getElementById('product-options');
+
+function toggleMenu() {
+  console.log(this);
+}
+
+productsTab.addEventListener('mouseover', toggleMenu);
