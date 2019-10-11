0e58bd3305637d316418e1f9b3da716e0c76cbc6
Teddi Woodson <teddiwood97@ufl.edu>
2019-10-11 14:36:10 -0400

    Add function to products tab

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // It is always helpful to use comments in your code!
 var productsTab = document.getElementById('products');
-var productsOptions = document.getElementById('product-options');
+var productsOptions = document.querySelector('.products');
 
 function toggleMenu() {
-  console.log(this);
+  productsOptions.classList.add('show-menu');
 }
 
 productsTab.addEventListener('mouseover', toggleMenu);
